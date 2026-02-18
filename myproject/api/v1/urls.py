211c3dcdde973034499c0abe085a5c0f3dd77394
abdo <as3669@fayoum.edu.eg>
2026-02-18 23:41:24 +0200

    using routers to creat the endpoint

--- myproject/api/v1/urls.py
+++ myproject/api/v1/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('users', UsersViewSet)
+router.register('bookings', BookingsViewSet)
+router.register('fields', FieldsViewSet)
 
 urlpatterns = [
-    path('users/', ApiUsers.as_view(), name='api_users'),
-    path('user/<int:pk>/', ApiUser.as_view(), name='api_user'),
-    path('bookings/', ApiBookings.as_view(), name='api_bookings'),
-    path('booking/<int:pk>/', ApiBooking.as_view(), name='api_booking'),
-    path('fields/', ApiFields.as_view(), name='api_fields'),
-    path('field/<int:pk>/', ApiField.as_view(), name='api_field'),
+    path('', include(router.urls)),
+    
 ]
--- myproject/api/v1/views.py
+++ myproject/api/v1/views.py
@@ -1,28 +1,16 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import UserSerializer, BookingsSerializer, FieldsSerializer
 from myapp.models import Users, Bookings, Fields
+from rest_framework.viewsets import ModelViewSet
 
-class ApiUsers(ListCreateAPIView):
-    queryset = Users.objects.all()
-    serializer_class = UserSerializer
-        
-class ApiUser(RetrieveUpdateDestroyAPIView):
+class UsersViewSet(ModelViewSet):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
 
-class ApiBookings(ListCreateAPIView):
-    queryset = Bookings.objects.all()
-    serializer_class = BookingsSerializer
-
-class ApiBooking(RetrieveUpdateDestroyAPIView):
+class BookingsViewSet(ModelViewSet):
     queryset = Bookings.objects.all()
     serializer_class = BookingsSerializer
         
-class ApiFields(ListCreateAPIView):
-    queryset = Fields.objects.all()
-    serializer_class = FieldsSerializer
-
-class ApiField(RetrieveUpdateDestroyAPIView):
+class FieldsViewSet(ModelViewSet):
     queryset = Fields.objects.all()
     serializer_class = FieldsSerializer
     
