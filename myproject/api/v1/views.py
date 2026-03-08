6a4e7a929f100b76562f4bcb6bafdff676dc3b12
abdo <as3669@fayoum.edu.eg>
2026-03-08 15:40:41 +0200

    Modify models and views of the project

--- myproject/api/v1/views.py
+++ myproject/api/v1/views.py
@@ -1,6 +1,30 @@
-from .serializers import UserSerializer, BookingsSerializer, FieldsSerializer
-from myapp.models import Users, Bookings, Fields
+from datetime import datetime
+from django.db.models import Q
+from .serializers import FieldImageSerializer, LoginDataSerializer, UserSerializer, BookingsSerializer, FieldsSerializer
+from myapp.models import FieldImages, Users, Bookings, Fields
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import status, permissions, serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from .permissions import IsOwner, IsPlayer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message": "User created successfully",
+            "user": serializer.data
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginDataView(TokenObtainPairView):
+    serializer_class = LoginDataSerializer
 
 class UsersViewSet(ModelViewSet):
     queryset = Users.objects.all()
@@ -9,8 +33,72 @@ class UsersViewSet(ModelViewSet):
 class BookingsViewSet(ModelViewSet):
     queryset = Bookings.objects.all()
     serializer_class = BookingsSerializer
+
+    def get_queryset(self):
+            user = self.request.user
+            
+            if user.user_type == 'owner':
+                return Bookings.objects.filter(field__owner=user).order_by('-created_at')
+            
+            return Bookings.objects.filter(user=user).order_by('-created_at')
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsPlayer()]
         
+        return [permissions.IsAuthenticated()]
+
+    def perform_create(self, serializer):
+        field = serializer.validated_data['field']
+        start_time = serializer.validated_data['start_time']
+        end_time = serializer.validated_data['end_time']
+        booking_date = serializer.validated_data['booking_date']
+
+        overlapping_bookings = Bookings.objects.filter(
+            field=field,
+            booking_date=booking_date,
+        ).exclude(
+            status='cancelled'
+        ).filter(
+            Q(start_time__lt=end_time, end_time__gt=start_time)
+        )
+
+        if overlapping_bookings.exists():
+            raise serializers.ValidationError("هذا الملعب محجوز بالفعل في هذا التوقيت!")
+
+        fmt = '%H:%M:%S'
+        t1 = datetime.strptime(str(start_time), fmt)
+        t2 = datetime.strptime(str(end_time), fmt)
+        
+        duration_hours = (t2 - t1).total_seconds() / 3600
+
+        if duration_hours <= 0:
+            raise serializers.ValidationError("وقت الانتهاء يجب أن يكون بعد وقت البدء!")
+
+        calculated_total_price = float(field.price_per_hour) * duration_hours
+
+        serializer.save(
+            user=self.request.user, 
+            total_price=calculated_total_price, 
+            status='pending'
+        )
+
 class FieldsViewSet(ModelViewSet):
     queryset = Fields.objects.all()
     serializer_class = FieldsSerializer
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsOwner()]
+        return [permissions.AllowAny()]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class FieldImagesViewSet(ModelViewSet):
+    queryset = FieldImages.objects.all()
+    serializer_class = FieldImageSerializer
     
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'destroy']:
+            return [IsOwner()]
+        return [permissions.AllowAny()]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-twghz^x)67py*046)m#rp%-2l^v9xcd5#0oe&ysc06pad=!hp(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'myapp',
     'api',
     'rest_framework',
@@ -44,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'myproject.urls'
 
@@ -79,7 +84,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mala3bna',
+        'NAME': 'mala3bna_db',
         'USER': 'postgres',
         'PASSWORD': '2004',
         'HOST': 'localhost',
@@ -110,8 +115,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Cairo'
+USE_TZ = True
 
 USE_I18N = True
 
@@ -122,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -135,6 +143,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', 
+    ),
 }
 
 SIMPLE_JWT = {
@@ -142,3 +153,4 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+AUTH_USER_MODEL = 'myapp.Users'
