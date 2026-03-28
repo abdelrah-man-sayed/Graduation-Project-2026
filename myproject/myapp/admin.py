fc5d1da5e36d867e372c39dee77e9afdee59a4bd
abdo <as3669@fayoum.edu.eg>
2026-03-29 01:23:31 +0200

    Fix structure: Move manage.py to root

--- myproject/myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Users, Fields, Bookings
-admin.site.register(Users)
-admin.site.register(Fields)
-admin.site.register(Bookings)
