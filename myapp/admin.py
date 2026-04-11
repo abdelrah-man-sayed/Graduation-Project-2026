2271408eafceed47f0f20c6c01cfda7e335a081a
abdo <as3669@fayoum.edu.eg>
2026-04-11 16:51:05 +0200

    Editing image field in admin site

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from .models import Users, Fields, Bookings, FieldImages
 
-# Register your models here.
-from .models import Users, Fields, Bookings
 admin.site.register(Users)
-admin.site.register(Fields)
 admin.site.register(Bookings)
+admin.site.register(FieldImages)
+
+class FieldImageInline(admin.TabularInline):
+    model = FieldImages
+    extra = 3
+
+@admin.register(Fields)
+class FieldsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'price_per_hour', 'location')
+    inlines = [FieldImageInline]
