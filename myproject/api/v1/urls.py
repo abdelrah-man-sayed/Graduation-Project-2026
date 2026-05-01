5ad99293d24d2c4c9fc4a1944b399f2319e3795d
“shahd“ <sa3722@fayoum.edu.eg>
2026-05-01 18:39:55 +0300

    Final: Added all API v1 logic files from master

---
+++ myproject/api/v1/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+router = DefaultRouter()
+
+router.register('users', views.UsersViewSet)
+router.register('bookings', views.BookingsViewSet)
+router.register('fields', views.FieldsViewSet)
+router.register('field_images', views.FieldImagesViewSet)
+
+urlpatterns = [
+    path('auth/signup/', views.signup, name='signup'),
+    path('auth/login/', views.LoginDataView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
+]
